5583f057772781a639a29f8d581899b0f6e81215
Chelsea <chels.thompson39@gmail.com>
2020-03-10 17:58:44 -0400

    fiddled with html and app.py

--- Mission-to-Mars/app.py
+++ Mission-to-Mars/app.py
@@ -11,14 +11,14 @@ mongo = PyMongo(app, uri="mongodb://localhost:27017/scrape_mars")
 
 @app.route("/")
 def index():
-    mars_scrape = mongo.mars_db.mars_scrape.find_one()
+    mars_scrape = mongo.db.mars_scrape.find_one()
 
     return render_template("index.html", mars_scrape=mars_scrape)
 
 
 @app.route("/scrape")
 def scraper():
-    mars_scrape = mongo.mars_db.mars_scrape
+    mars_scrape = mongo.db.mars_scrape
     mars_scrape_data = scrape_mars.scrape()
     mars_scrape.update({}, mars_scrape_data, upsert=True)
     return redirect("/", code=302)
